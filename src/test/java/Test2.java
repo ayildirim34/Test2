c03df9bf8f11dce66765e7ecf70e0bcb35fc36c8
nagihan <nagihan2957@protonmail.com>
2022-04-21 17:02:23 +0200

    3

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,5 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("basla");
         System.out.println("devam");
+        System.out.println("devam2");
     }
 }
